99302777f969fd122b5ea9599c7f78450c504a93
sangkanfaiq <sangkanfaiq@gmail.com>
2022-08-02 23:30:10 +0700

    feat: add Head title

--- src/pages/chatroom/index.js
+++ src/pages/chatroom/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import NavbarLogin from "../../components/navbarLogin";
 import ChatroomLayout from "../../layouts/chatroom";
+import Head from 'next/head'
 
 const Chatroom = () => {
   return (
     <>
+      <Head>
+        <title>Chatroom - Coffee Shop</title>
+      </Head>
       <NavbarLogin />
       <ChatroomLayout />
     </>
